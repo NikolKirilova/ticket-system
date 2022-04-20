69c2a3a275999ea22f90b37a140e2abe720f0067
NikolKirilova <kirilova@mail.bg>
2022-04-20 21:42:41 +0300

    Edit Register, Login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,199 +1,220 @@
 <template>
   <div class="login">
     <div class="login-box">
-    <div class="left-side">
-      <h2>Login</h2>
-      <p>Sign in and control your work.</p>
-    </div>
-    <div class="form-wrapper">
-     
-    <form @submit.prevent="loginHandler">
-       
-      <div class="field">
-        <!-- <label for="email">Email</label> -->
-        <input type="email" id="email" placeholder="Enter email address" v-model="email"  @input="$v.email.$touch()"/>
-        <div v-if="$v.email.$error">
-       <div class="field-error" v-if="!$v.email.required">Field is required!</div>
-       <div class="field-error" v-if="!$v.email.email">Email is invalid!</div>
-        </div>
-      <template v-if="errors && errors.email">
-        <div class="field-error" v-for="(error, index) in errors.email" :key="index">
-          {{error}}
-        </div>
-        </template>
+      <div class="left-side">
+        <h2>Login</h2>
+        <p>Sign in and control your work.</p>
       </div>
-      <div class="field">
-        <!-- <label for="password">Password</label> -->
-        <input
-          type="password"
-          id="password"
-          placeholder="Enter Password"
-          v-model="password"
-           @input="$v.password.$touch()"
-        />
-        <div v-if="$v.password.$error">
-         <div class="field-error" v-if="!$v.password.required">Field is required!</div>
-       <div class="field-error" v-if="!$v.password.minLength">Password should be at least 8 characters long!</div>
-        </div>
-         <template v-if="errors && errors.password">
-        <div class="field-error" v-for="(error, index) in errors.password" :key="index">
-          {{error}}
-        </div>
-        </template>
+      <div class="form-wrapper">
+        <form @submit.prevent="loginHandler">
+          <div class="field">
+            <!-- <label for="email">Email</label> -->
+            <input
+              type="email"
+              id="email"
+              placeholder="Enter email address"
+              v-model="email"
+              @input="$v.email.$touch()"
+            />
+            <div v-if="$v.email.$error">
+              <div class="field-error" v-if="!$v.email.required">
+                Field is required!
+              </div>
+              <div class="field-error" v-if="!$v.email.email">
+                Email is invalid!
+              </div>
+            </div>
+            <template v-if="errors && errors.email">
+              <div
+                class="field-error"
+                v-for="(error, index) in errors.email"
+                :key="index"
+              >
+                {{ error }}
+              </div>
+            </template>
+          </div>
+          <div class="field">
+            <!-- <label for="password">Password</label> -->
+            <input
+              type="password"
+              id="password"
+              placeholder="Enter Password"
+              v-model="password"
+              @input="$v.password.$touch()"
+            />
+            <div v-if="$v.password.$error">
+              <div class="field-error" v-if="!$v.password.required">
+                Field is required!
+              </div>
+              <div class="field-error" v-if="!$v.password.minLength">
+                Password should be at least 8 characters long!
+              </div>
+            </div>
+            <template v-if="errors && errors.password">
+              <div
+                class="field-error"
+                v-for="(error, index) in errors.password"
+                :key="index"
+              >
+                {{ error }}
+              </div>
+            </template>
+          </div>
+          <div class="forget-password">
+            <router-link to="/register">Forget Password?</router-link>
+          </div>
+
+          <div class="actions">
+            <button :disabled="$v.$invalid">Login</button>
+            <span>or</span>
+            <router-link to="/">Register</router-link>
+          </div>
+        </form>
       </div>
-      <div  class="forget-password">
-       <router-link to="/register" >Forget Password?</router-link>
-
-      </div>
-
-        <div class="actions">
-          
-      <button :disabled="$v.$invalid" >Login</button>
-       <router-link to="/register" >Register</router-link>
-          </div>  
-       
-        
-   
-    </form>
     </div>
-    </div>
-
   </div>
 </template>
 
 <script>
-import authMixin from '@/mixins/auth'
-import { required, email, minLength} from 'vuelidate/lib/validators'
-  
+import authMixin from "@/mixins/auth";
+import { required, email, minLength } from "vuelidate/lib/validators";
+
 export default {
   name: "Login",
   mixins: [authMixin],
   data() {
-    return {   
+    return {
       email: null,
       password: null,
-      errors: null
+      errors: null,
     };
   },
   methods: {
     loginHandler() {
-       let dataToSend = {          
-           email: this.email,
-           password: this.password 
-       }
-       this.login(dataToSend)
+      let dataToSend = {
+        email: this.email,
+        password: this.password,
+      };
+      this.login(dataToSend)
         .then(() => {
-            this.$router.push('/my-profile');
+          this.$router.push("/my-profile");
         })
-        .catch(err => {
-          this.errors = err.response.data.errors; 
+        .catch((err) => {
+          this.errors = err.response.data.errors;
           setTimeout(() => {
-              this.errors = null;
+            this.errors = null;
           }, 5000);
-            })
-    }
+        });
+    },
   },
-   validations: {   
-     email: {
-        required,
-        email
-     },
-     password: {
-         required,
-         minLength: minLength(8)
-     } 
-  }
+  validations: {
+    email: {
+      required,
+      email,
+    },
+    password: {
+      required,
+      minLength: minLength(8),
+    },
+  },
 };
 </script>
 
 <style scoped>
- 
-.login{
- position:absolute; 
- top:300px;
- width: 520px;
-margin-left: auto;
-margin-right: auto;
-left: 0;
-right: 0;
-text-align: center;
-   
+.login {
+  position: absolute;
+  top: 145px;
+  width: 600px;
+  margin-left: auto;
+  margin-right: auto;
+  left: 0;
+  right: 0;
+  text-align: center;
 }
-.login-box{
-  display:flex;
-  justify-content:center;
+.login-box {
+  display: flex;
+  justify-content: center;
   box-shadow: 0px 0px 4px 1px #ebe6e6;
 }
-.left-side{
-  width:220px;
-  background:#47b2e6;
-     color: #fff;
-    text-align: left;
-    padding: 25px;
-    border-top-left-radius:4px;
-    border-bottom-left-radius: 4px;
+.left-side {
+  width: 220px;
+  background: #47b2e6;
+  color: #fff;
+  text-align: left;
+  padding: 25px;
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px;
 }
-.form-wrapper{
-  width:300px;
+.form-wrapper {
+  width: 380px;
 }
-    form {     
-        margin: 50px auto;
-    }
-    .field {
-        width: 100%;
-        margin-bottom: 20px;
-    }
-    .field label {
-        display: block;
-        font-size:18px;
-        margin-bottom: 5px;
-    } 
-    /* .field input {     
+form {
+  margin: 50px auto;
+}
+.field {
+  width: 100%;
+  margin-bottom: 20px;
+}
+.field label {
+  display: block;
+  font-size: 18px;
+  margin-bottom: 5px;
+}
+/* .field input {     
          font-size:18px;
          padding:5px 18px;
              border-radius: 15px;
     border: 1px solid #bfb7b7;
     } */
 
-    .field-error {
-        border-radius: 8px;
-        border: 1px solid #ff0000;
-        background-color: #f0e5e5;
-        color:red;
-        padding:5px 10px;
-        font-size:16px;
-        margin-top:5px;
-        text-align: center;
-    }
-    button{
-        font-size:24px;
-    }
+.field-error {
+  border-radius: 8px;
+  border: 1px solid #ff0000;
+  background-color: #f0e5e5;
+  color: red;
+  padding: 5px 10px;
+  font-size: 16px;
+  margin-top: 5px;
+  text-align: center;
+}
+ 
 
-    .actions{
-          display: flex;
-    width: 75%;
-    margin: auto;
+.actions {
+    display: flex;
+    width: 60%;
     justify-content: space-between;
     margin-top: 45px;
-    align-items:center;
-    }
-    .actions button{
-          color: #47b2e6;
+    align-items: center;
+    margin-left: 40px;
+}
+.actions button {
+    color: #47b2e6;
     background: #fff;
-    border: 1px solid #ddd;
-    border-radius: 15px;
-    padding: 8px 14px;
-    font-size: 16px;
-     box-shadow: 0px 0px 4px 1px #ebe6e6;
-    }
-    .actions a, .forget-password a {
-      text-decoration: none;
-    font-weight: 600;
-          color: #47b2e6;
+    border-radius: 20px;
+    padding: 11px 14px;
+    font-size: 18px;
+    box-shadow: 0px 0px 4px 1px rgb(158 147 147 / 23%);
+    cursor:pointer;
+}
+.actions a,
+.forget-password a {
+  text-decoration: none;
+  font-weight: 600;
+  color: #47b2e6;
+  font-size: 18px;
+    cursor:pointer;
 
-    }
-    .forget-password{
-          text-align: left;
-    margin-left: 30px;
-    }
+}
+.forget-password {
+  text-align: left;
+  margin-left: 30px;
+}
+h2 {
+  font-size: 32px;
+    letter-spacing: 0.5px;
+}
+p{
+  font-size: 18px;
+}
 </style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,73 +1,122 @@
 <template>
   <div class="register">
+      <div class="register-box">
+      <div class="left-side">
+        <h2>Register</h2>
+        <p>Sign up and control your work.</p>
+      </div>
+      <div class="form-wrapper">
     <form @submit.prevent="registerHandler">
       <div class="field">
-        <label for="name">Name</label>
-        <input type="text" id="name" placeholder="Name..." v-model="name" @input="$v.name.$touch()"/>
+        <!-- <label for="name">Name</label> -->
+        <input
+          type="text"
+          id="name"
+          placeholder="Enter your name"
+          v-model="name"
+          @input="$v.name.$touch()"
+        />
         <div v-if="$v.name.error">
-        <div class="field-error" v-if="!$v.name.required">Field is required!</div>
+          <div class="field-error" v-if="!$v.name.required">
+            Field is required!
+          </div>
         </div>
         <template v-if="errors && errors.name">
-        <div class="field-error" v-for="(error, index) in errors.name" :key="index">
-          {{error}}
-        </div>
+          <div
+            class="field-error"
+            v-for="(error, index) in errors.name"
+            :key="index"
+          >
+            {{ error }}
+          </div>
         </template>
       </div>
       <div class="field">
-        <label for="email">Email</label>
-        <input type="email" id="email" placeholder="Email..." v-model="email"  @input="$v.email.$touch()"/>
+        <!-- <label for="email">Email</label> -->
+        <input
+          type="email"
+          id="email"
+          placeholder="Enter your email"
+          v-model="email"
+          @input="$v.email.$touch()"
+        />
         <div v-if="$v.email.$error">
-       <div class="field-error" v-if="!$v.email.required">Field is required!</div>
-       <div class="field-error" v-if="!$v.email.email">Email is invalid!</div>
+          <div class="field-error" v-if="!$v.email.required">
+            Field is required!
+          </div>
+          <div class="field-error" v-if="!$v.email.email">
+            Email is invalid!
+          </div>
         </div>
-      <template v-if="errors && errors.email">
-        <div class="field-error" v-for="(error, index) in errors.email" :key="index">
-          {{error}}
-        </div>
+        <template v-if="errors && errors.email">
+          <div
+            class="field-error"
+            v-for="(error, index) in errors.email"
+            :key="index"
+          >
+            {{ error }}
+          </div>
         </template>
       </div>
       <div class="field">
-        <label for="password">Password</label>
+        <!-- <label for="password">Password</label> -->
         <input
           type="password"
           id="password"
-          placeholder="Password..."
+          placeholder="Enter your password"
           v-model="password"
-           @input="$v.password.$touch()"
+          @input="$v.password.$touch()"
         />
         <div v-if="$v.password.$error">
-         <div class="field-error" v-if="!$v.password.required">Field is required!</div>
-       <div class="field-error" v-if="!$v.password.minLength">Password should be at least 8 characters long!</div>
+          <div class="field-error" v-if="!$v.password.required">
+            Field is required!
+          </div>
+          <div class="field-error" v-if="!$v.password.minLength">
+            Password should be at least 8 characters long!
+          </div>
         </div>
-         <template v-if="errors && errors.password">
-        <div class="field-error" v-for="(error, index) in errors.password" :key="index">
-          {{error}}
-        </div>
+        <template v-if="errors && errors.password">
+          <div
+            class="field-error"
+            v-for="(error, index) in errors.password"
+            :key="index"
+          >
+            {{ error }}
+          </div>
         </template>
       </div>
-            <div class="field">
-        <label for="confirm-password">Confirm Password</label>
+      <div class="field">
+        <!-- <label for="confirm-password">Confirm Password</label> -->
         <input
           type="password"
           id="confirm-password"
-          placeholder="Confirm Password..."
+          placeholder="Confirm Password"
           v-model="confirmPassword"
           @input="$v.confirmPassword.$touch()"
         />
         <div v-if="!$v.confirmPassword.$error">
-        <div class="field-error" v-if="!$v.confirmPassword.sameAsPassword"> Password should match!</div>
+          <div class="field-error" v-if="!$v.confirmPassword.sameAsPassword">
+            Password should match!
+          </div>
         </div>
       </div>
+        <div class="actions">
       <button :disabled="$v.$invalid">Register</button>
-   
+      <span>or</span>
+      <router-link to="/login">Login</router-link>
+  </div>
+
     </form>
+  </div>
+  </div>
+
   </div>
 </template>
 
 <script>
-import authMixin from '@/mixins/auth'
-import { required, email, minLength, sameAs} from 'vuelidate/lib/validators'
-  
+import authMixin from "@/mixins/auth";
+import { required, email, minLength, sameAs } from "vuelidate/lib/validators";
+
 export default {
   name: "Register",
   mixins: [authMixin],
@@ -77,79 +126,138 @@ export default {
       email: null,
       password: null,
       confirmPassword: null,
-      errors: null
+      errors: null,
     };
   },
   methods: {
     registerHandler() {
-       let dataToSend = {
-           name: this.name,
-           email: this.email,
-           password: this.password,
-           password_confirmation: this.confirmPassword
-       }
-       this.register(dataToSend)
+      let dataToSend = {
+        name: this.name,
+        email: this.email,
+        password: this.password,
+        password_confirmation: this.confirmPassword,
+      };
+      this.register(dataToSend)
         .then(() => {
-            this.$router.push('/login')
+          this.$router.push("/login");
         })
-        .catch(err => {
-          this.errors = err.response.data.errors; 
+        .catch((err) => {
+          this.errors = err.response.data.errors;
           setTimeout(() => {
-              this.errors = null;
+            this.errors = null;
           }, 5000);
-            })
-    }
-  },
-   validations: {
-    name: {
-      required      
+        });
     },
-     email: {
-        required,
-        email
-     },
-     password: {
-         required,
-         minLength: minLength(8)
-     },
-     confirmPassword: {
-         sameAsPassword: sameAs('password')
-     }
-  }
+  },
+  validations: {
+    name: {
+      required,
+    },
+    email: {
+      required,
+      email,
+    },
+    password: {
+      required,
+      minLength: minLength(8),
+    },
+    confirmPassword: {
+      sameAsPassword: sameAs("password"),
+    },
+  },
 };
 </script>
 
 <style scoped>
-    form {
-        width: 800px;
-        margin: 50px auto;
-    }
-    .field {
-        width: 100%;
-        margin-bottom: 20px;
-    }
-    .field label {
-        display: block;
-        font-size:18px;
-        margin-bottom: 5px;
-    } 
-    .field input {
-        width: 100%;
-         font-size:18px;
-         padding:5px 18px;
-    }
+.register {
+  position: absolute;
+  top: 145px;
+  width: 600px;
+  margin-left: auto;
+  margin-right: auto;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+.register-box {
+  display: flex;
+  justify-content: center;
+  box-shadow: 0px 0px 4px 1px #ebe6e6;
+}
+.left-side {
+  width: 220px;
+  background: #47b2e6;
+  color: #fff;
+  text-align: left;
+  padding: 25px;
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px;
+}
+.form-wrapper {
+  width: 380px;
+}
+form {
+  
+  margin: 50px auto;
+}
+.field {
+  width: 100%;
+  margin-bottom: 20px;
+}
+.field label {
+  display: block;
+  font-size: 18px;
+  margin-bottom: 5px;
+}
+/* .field input {
+  width: 100%;
+  font-size: 18px;
+  padding: 5px 18px;
+} */
 
-    .field-error {
-        border-radius: 8px;
-        border: 1px solid #ff0000;
-        background-color: #f0e5e5;
-        color:red;
-        padding:5px 10px;
-        font-size:16px;
-        margin-top:5px;
-        text-align: center;
-    }
-    button{
-        font-size:24px;
-    }
+.field-error {
+  border-radius: 8px;
+  border: 1px solid #ff0000;
+  background-color: #f0e5e5;
+  color: red;
+  padding: 5px 10px;
+  font-size: 16px;
+  margin-top: 5px;
+  text-align: center;
+}
+ 
+.actions {
+      display: flex;
+    width: 60%;
+    justify-content: space-between;
+    margin-top: 45px;
+    align-items: center;
+    margin-left: 40px;
+}
+.actions button {
+    color: #47b2e6;
+    background: #fff;
+    border-radius: 20px;
+    padding: 11px 14px;
+    font-size: 18px;
+    box-shadow: 0px 0px 4px 1px rgb(158 147 147 / 23%);
+    cursor:pointer;
+
+}
+.actions a,
+.forget-password a {
+  text-decoration: none;
+  font-weight: 600;
+  color: #47b2e6;
+  font-size: 18px;
+    cursor:pointer;
+
+}
+h2 {
+  font-size: 32px;
+    letter-spacing: 0.5px;
+}
+p{
+  font-size: 18px;
+}
 </style>
